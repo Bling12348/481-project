dba6628b9d85a0b16839bd09626304899f50e6ca
nkaspar3 <nicholas.j.kasparek@gmail.com>
2025-12-01 18:25:57 -0500

    cart populating working

--- Checkout/app.js
+++ Checkout/app.js
@@ -23,9 +23,6 @@ function displayCart(){
         <div class="item-entry row g-4 align-items-center">
             <div class="foodname p-4 col-sm text-center mr-3">${item.name}</div>
             <div class="price col-sm align-self-center mr-3">${item.price}</div>
-            <div class="col-sm align-self-center mr-3">
-                <button class="btn btn-dark btn-sm" data-add>Remove Item</button>
-            </div>
         </div>
         `;
         cartContainer.appendChild(cartItem);
